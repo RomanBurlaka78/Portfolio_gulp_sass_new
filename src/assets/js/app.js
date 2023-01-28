55cd4c008ffe9f055c5a2dd921273a4637ad8b5e
Roman Burlaka <burlakarv@gmail.com>
2023-01-28 20:27:34 +0100

    topsidebar changes

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,6 +4,8 @@ const burger = document.querySelector('.burger');
 const navBarNav = document.querySelector('.nav_list');
 const navLink = document.querySelectorAll('.nav_link');
 const body = document.querySelector('body');
+// const sidebar = document.querySelector('.sidebar');
+// const showSidebar = document.querySelector('.profile_avatar--mobile--span')
 
 
 burger.addEventListener('click', ()=> {
@@ -11,6 +13,7 @@ burger.addEventListener('click', ()=> {
     burger.classList.toggle('active');
     navBarNav.classList.toggle('active');
     body.classList.toggle('locked')
+    // sidebar.classList.remove('show')
    
    
 })
@@ -24,5 +27,28 @@ navLink.forEach((item)=> {
     })
 
 })
+
+
+
+    const headerAvatar = document.querySelector(".header__avatar");
+    const sidebar = document.querySelector(".sidebar");
+    headerAvatar.addEventListener("click", ()=>{
+        sidebar.classList.toggle("sidebar--show");
+        
+    })
+     
+
+window.addEventListener('resize', function(event) {
+    sidebar.classList.remove("sidebar--show");
+    
+}, true);
+    
+   
+    
+    
+
+
+
+
 
 
